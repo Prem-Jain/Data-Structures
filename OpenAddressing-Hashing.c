7675b75dd32ce16ca46550ab1d2bcf3e23d866b4
J Prem <88187995+Prem-Jain@users.noreply.github.com>
2022-10-09 15:28:07 +0530

    Update OpenAddressing-Hashing.c

--- OpenAddressing-Hashing.c
+++ OpenAddressing-Hashing.c
@@ -8,42 +8,47 @@ struct node
     struct node *next;
 };
 
-struct node *head[size]={NULL},*c;
+struct node *head[size] = {NULL}, *c;
+
+int hash(int key)
+{
+    return (key % n);
+}
 
 void insert()
 {
-    int i,key;
+    int i, key;
     struct node *newnode;
     printf("Enter value to insert into hash table : ");
-    scanf("%d",&key);
-    i = key%n;
+    scanf("%d", &key);
+    i = hash(key);
     newnode = (struct node*)malloc(sizeof(struct node));
-    newnode->data = key;
-    newnode->next = NULL;
-    if(head[i]==NULL)
-        head[i]=newnode;
+    newnode -> data = key;
+    newnode -> next = NULL;
+    if(head[i] == NULL)
+        head[i] = newnode;
     else
     {
-        c=head[i];
-        while(c->next!=NULL)
-            c=c->next;
-        c->next = newnode;
+        c = head[i];
+        while(c -> next!=NULL)
+            c = c -> next;
+        c -> next = newnode;
     }
 }
 
 void search()
 {
-    int key, index;
+    int key, i;
     printf("Enter element to be searched : ");
-    scanf("%d",&key);
-    index = key%n;
-    if(head[index]==NULL)
+    scanf("%d", &key);
+    i = hash(key)
+    if(head[i] == NULL)
         printf("Element not found\n");
     else
     {
-        for(c=head[index];c!=NULL;c=c->next)
+        for(c = head[i]; c != NULL; c = c->next)
         {
-            if(c->data==key)
+            if(c -> data == key)
             {
                 printf("Search element found\n");
                 return;
@@ -56,15 +61,15 @@ void search()
 void display()
 {
     int i;
-    for(i=0;i<n;i++)
+    for(i = 0; i < n; i++)
     {
-        printf("Entries at index %d : ",i);
-        if(head[i]==NULL)
+        printf("Entries at index %d : ", i);
+        if(head[i] == NULL)
             printf("No hash entry\n");
         else
         {
-            for(c=head[i];c!=NULL;c=c->next)
-                printf("%d -> ",c->data);
+            for(c = head[i]; c != NULL; c = c -> next)
+                printf("%d -> ", c -> data);
             printf("\n");
         }
     }
@@ -72,13 +77,13 @@ void display()
 
 void main()
 {
-    int op,i;
+    int op, i;
     printf("Enter the table size : ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     while(1)
     {
         printf("1.Insert\t2.Display\t3.Search\t4.Exit\n");
-        scanf("%d",&op);
+        scanf("%d", &op);
         switch(op)
         {
             case 1: insert(); display(); break;
